2057634873e6a7567c1326080e59e2ca4c07bba5
Unknown <seranmoreno500@gmail.com>
2018-04-07 13:21:20 +0200

    Primer commit
    El resto son gilipolleces, solo importa el Pong

---
+++ Con la polla/Assets/Script/PlayerColision.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class PlayerColision : MonoBehaviour {
+
+    public PlayerMovement movement;
+    public Transform transform;
+
+
+    void OnCollisionEnter(Collision collisionInfo)
+    {
+        if (collisionInfo.collider.tag == "Obstacle")
+        {
+            movement.enabled = false;
+
+        }
+    }
+
+   
+
+}
---
+++ Pong/Assets/Scripts/BallMovement.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class BallMovement : MonoBehaviour {
+
+    private Vector3 posicionInicial = Vector3.zero;
+    public float fuerzaInicial = 0f;
+    public Transform transform;
+    public Rigidbody rigidbody;
+
+
+    void Start () {
+        
+    }
+	
+	void Update () {
+		
+	}
+}
