5e5113052175aac9240609cda6b04bc122a3ddfd
Unknown <seranmoreno500@gmail.com>
2018-07-14 18:55:48 +0200

    Inicado Tower Defense

---
+++ Tower Defense practice/Assets/EnemySpawn.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawn : MonoBehaviour {
+
+    private float waveCountdown = 5f;
+    private float enemyCountdown = 1f;
+    private float enemys = 1f;
+
+    private void Update()
+    {
+
+        waveCountdown -= 1 * Time.deltaTime;
+
+        if (waveCountdown == 0)
+        {
+            SpawnWave();
+        }
+
+    }
+
+    private void SpawnWave()
+    {
+
+    }
+
+}
---
+++ Tower Defense practice/Assets/Scripts/Enemy.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+
+public class Enemy : MonoBehaviour {
+
+    public float speed = 10f;
+    private Transform target;
+    private int index = 0;
+
+    void Start()
+    {
+        //Usamos la variable para ir cambiandola por el objetivos al que queremos ir
+        target = WayPoints.points[index];
+    }
+	
+	void Update ()
+    {
+        //Conseguiismos èl vector que queremso al restar de esta manera
+        Vector3 direc = target.position - transform.position;
+        //Creamso un vector unitario, le aplciamos la velocidad elegida y Time.dentaTime
+        transform.Translate(direc.normalized * speed * Time.deltaTime);
+
+        //Para cambiar el objetivo cuando es el mismo se cambia
+        //No se pone que sean iguales ya que se pueden dar distintos fallos al llegar
+        if (Vector3.Distance(transform.position, target.position) <= 0.4f)
+        {
+            GetNextWaypoint();
+        }
+    }
+    
+    //Cuando está muy cerca cogemos el siguiente y a correr
+    void GetNextWaypoint()
+    {
+        if (index >= WayPoints.points.Length - 1)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        index++;
+        target = target = WayPoints.points[index];
+    }
+}
